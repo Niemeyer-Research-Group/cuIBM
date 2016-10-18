7f113e285665cbb87c0fd34f6888c914f195d02a
Christopher Minar <majorminar137@gmail.com>
2016-10-18 14:30:10 -0700

    updating run_Viv.sh

--- scripts/shell/run_viv.sh
+++ scripts/shell/run_viv.sh
@@ -4,7 +4,7 @@
 CUIBM_DIR=/scratch/src/cuIBM
 #Run Lid Driven Cavity Validation
 cd /scratch/src/cuIBM/src
-for var1 in 4 5 6 7 8
+for var1 in 3 4 5 6 7 8
 do
 	echo "Running VIV for Ured " $var1
 	make vivUred$var1
