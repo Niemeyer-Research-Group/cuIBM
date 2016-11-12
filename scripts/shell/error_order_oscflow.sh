7ab562fe476d1b39e3f5b5390ab36ce99e90ed67
Christopher Minar <majorminar137@gmail.com>
2016-11-11 16:55:42 -0800

    changes to oscerror shell script

--- scripts/shell/error_order_oscflow.sh
+++ scripts/shell/error_order_oscflow.sh
@@ -4,21 +4,13 @@
 CUIBM_DIR=/scratch/src/cuIBM
 #Run Lid Driven Cavity Validation
 cd /scratch/src/cuIBM/src
-for var1 in 01 015625 03125 0625 
+for var1 in 015625 02 03125 0625 
 do
-	for var2 in external
-	do
-	echo "ERROR_ORDER_OSCFLOW.SH: Running "$var2$var1"\n"
-	#/scratch/src/cuIBM/bin/cuIBM -caseFolder /scratch/src/cuIBM/validation/error/oscflow/$var2$var1
-	done
-done
-for var1 in 03125 #015625 02 03125 0625 
-do
-	for var2 in external
+	for var2 in external embedded
 	do
 	echo "ERROR_ORDER_OSCFLOW.SH: Running "$var2$var1"\n"
 	echo /scratch/src/cuIBM/validation/error/oscflow/$var2$var1
 	/scratch/src/cuIBM/bin/cuIBM -caseFolder /scratch/src/cuIBM/validation/error/oscflow/$var2$var1
 	done
 done
-#/scratch/src/cuIBM/scripts/validation/error_order_oscflow.py
+/scratch/src/cuIBM/scripts/validation/error_order_oscflow.py
